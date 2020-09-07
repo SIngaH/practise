592f7a83fd3970412cde6f1ea2db223a5bb8ad17
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-09-07 10:45:42 +0200

    sektion 1 from terminsprøve

---
+++ animations/section1-terminsprøve/index.js
@@ -0,0 +1,27 @@
+// let boxes = document.querySelectorAll(".box img");
+// for (let i = 0; i < boxes.length; i++) {
+//     boxes[i].addEventListener("mouseover", (e)=>{
+//         haha(e)
+//     });
+//     boxes[i].addEventListener("mouseout", (e)=>{
+//         hihi(e)
+//     });
+// }
+// function haha(e){
+//     let img = e.path[1].children[0];
+//     let box = e.path[1].children[1];
+//     // console.log(img)
+//     // console.log(box)
+//     box.style.display = "block";
+//     img.style.display = "none";
+
+// }
+// function hihi(e){
+//     let img = e.path[1].children[0];
+//     let box = e.path[1].children[1];
+//     box.style.display = "none";
+//     img.style.display = "block";
+//     console.log(img)
+//     console.log(box)
+
+// }
