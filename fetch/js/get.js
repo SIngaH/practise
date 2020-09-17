5a9df5d52ff9f8ceb6cc100f51fd55844702401f
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-09-17 09:21:22 +0200

    deleted comments

--- fetch/js/get.js
+++ fetch/js/get.js
@@ -17,26 +17,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
             <p>` + result[0].asset + ` for no reason</p>
         `;
     });
+
 });
-/* this one worked before but not there is no longer anything on this address on the api */
-// document.addEventListener("DOMContentLoaded", ()=>{
-//     let putHere = document.querySelector(".get");
-//     fetch("http://localhost:4000/events", {
-//         "method": "GET",
-//         headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded'
-//         }
-//     })
-//     .then (function(response){
-//         return response.json();
-//     })
-//     .then (function(result){
-//         console.log(result);
-//         putHere.innerHTML = `
-//             <h1>` + result.eventName + `</h1>
-//             <p>` + result.eventDescription + `</p>
-//             <p>` + result.location + `</p>
-//             <p>` + result.asset + ` for no reason</p>
-//             `;
-//     });
-// });
+
